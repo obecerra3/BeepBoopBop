90ef135db566d35f649b5ccb334e032c8209bb75
Oscar Becerra <obecerra@Bachelor.local>
2017-11-04 11:44:08 -0400

    commit 4 - Basic Layout of workspace complete

---
+++ Beep Boop Bop/Scripts/AimJoystick.swift
@@ -0,0 +1,37 @@
+//
+//  AimJoystick.swift
+//  Beep Boop Bop
+//
+//  Created by Oscar Becerra on 11/4/17.
+//  Copyright © 2017 Oscar Becerra. All rights reserved.
+//
+
+import SpriteKit
+
+class AimJoystick: AnalogJoystick {
+    
+    let dataX = DataBank()
+    var width = CGFloat()
+    var height = CGFloat()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init() {
+        let substrateX = AnalogJoystickSubstrate(diameter: 100, color: .blue, image: UIImage(named: "jSubstrate"))
+        let stickX = AnalogJoystickStick(diameter: 20, color: .yellow, image: UIImage(named: "jStick"))
+        super.init(substrate: substrateX , stick: stickX)
+        
+        width = CGFloat(dataX.defaults.float(forKey: "width"))
+        height = CGFloat(dataX.defaults.float(forKey: "height"))
+        
+        
+        self.substrate.diameter = width * 0.15
+        self.stick.diameter = width * 0.1
+        self.position = CGPoint(x: width * 0.37 , y: height * -0.29)
+    }
+    
+    
+    
+}
---
+++ Beep Boop Bop/Scripts/DataBank.swift
@@ -0,0 +1,20 @@
+//
+//  DataBank.swift
+//  Beep Boop Bop
+//
+//  Created by Oscar Becerra on 11/4/17.
+//  Copyright © 2017 Oscar Becerra. All rights reserved.
+//
+
+import SpriteKit
+
+class DataBank {
+    
+    let defaults = UserDefaults.standard
+    
+    init() {
+        
+    }
+    
+}
+
